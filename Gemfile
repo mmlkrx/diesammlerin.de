c3b9343056f69fff58371b1b5a56e937dae8bddb
Matthias Loker <mmloker@gmail.com>
2016-02-07 17:30:17 +0100

    Enable gh-pages deployment

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
+gem 'middleman-deploy', github: 'middleman-contrib/middleman-deploy', branch: 'master'
--- config.rb
+++ config.rb
@@ -1,44 +1,17 @@
-###
-# Page options, layouts, aliases and proxies
-###
+page "CNAME", layout: false
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.build_before = true
+end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  set :relative_links, true
 end
